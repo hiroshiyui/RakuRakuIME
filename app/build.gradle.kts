0b90f07ed965dec7a0ba883d314f889579e7c884
Hui-Hong You <hiroshi@ghostsinthelab.org>
2026-04-21 01:21:31 +0800

    Release 1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,8 +41,8 @@ android {
         applicationId = "org.ghostsinthelab.app.rakurakuime"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0.0"
+        versionCode = 3
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
